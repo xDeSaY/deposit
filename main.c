629329e084270bc7797db761171e0457bde1693c
Daniil Podolnitskiy <daniil.podolnizkiy@yandex.ru>
2017-03-04 00:41:40 +0700

    implementation for correctness checks

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+int check(int day, int sum)
+{
+	if((day > 365)) {
+		return -1;
+	}
+	if(sum<1000) {
+		return -1;
+	}
+	return 0;
+}
+
 int main()
 {
 	int day, sum;
@@ -9,6 +20,16 @@ int main()
 
 	printf("Enter the deposit term: ");
 	scanf("%d", &day);
+
+	while(check(day, sum) == -1) {
+		printf("Data is entered incorrectly\n");
+
+		printf("Enter the deposit amount: ");
+		scanf("%d", &sum);
+
+		printf("Enter the deposit term: ");
+		scanf("%d", &day);
+	}
 
 	return 0;
 }
